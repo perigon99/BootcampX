b4dd2d780921ed69e6add73797916cb1047ff0c6
Sebastien <perigon@hotmail.com>
2021-02-03 19:55:11 +0000

    SQL injection prevention

--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -6,16 +6,21 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-//const process = process.argv.slice(2)
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+
+const cohortName = process.argv[2];
+
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
+
+const teacherQuery = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;`
+
+pool.query(teacherQuery,values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
